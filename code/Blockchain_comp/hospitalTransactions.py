7c3b7421b04c5f0f0983ceec68be21aae7a4e8b5
nicole <78940989+niknacks@users.noreply.github.com>
2022-11-23 19:07:14 -0500

    test add blockchain base

---
+++ code/Blockchain_comp/hospitalTransactions.py
@@ -0,0 +1,17 @@
+import json
+import base64
+from algosdk import account, mnemonic, constants
+from algosdk.v2client import algod
+from algosdk.future import transaction
+
+# used to allow hospitals to send and update their patient data
+def uploadPatientData():
+    pass
+
+# send patient data to model
+def sendTransaction():
+    pass
+
+# revieve updates to patient data from model
+def recieveTransaction():
+    pass
