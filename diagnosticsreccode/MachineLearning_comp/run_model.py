6cd1b3c5498df2c3cf8215e2f607e460b3673b07
charishruthi <charishruthi@gmail.com>
2022-12-02 11:59:50 -0500

    A cleaned up transaction committed wherein max node is picked

--- diagnosticsreccode/MachineLearning_comp/run_model.py
+++ diagnosticsreccode/MachineLearning_comp/run_model.py
@@ -31,15 +31,30 @@ def transform_data(df):
 def get_models():
 	'''
 	Define here what models you want to run - maybe this can be parameterized
+	First number in list is stored on the blockchain
 	'''
 	random_state = 30
 
-	models = [
-		['Logistic Regression', LogisticRegression(random_state = random_state, solver='liblinear')],
-		['Decision Tree',DecisionTreeClassifier(random_state = random_state)],
-		['Random Forest', RandomForestClassifier(random_state = random_state)],
-	]
+	models = {
+		1: ['Logistic Regression', LogisticRegression(random_state = random_state, solver='liblinear')],
+		2: ['Decision Tree',DecisionTreeClassifier(random_state = random_state)],
+		3: ['Random Forest', RandomForestClassifier(random_state = random_state)],
+	}
 	return models
+
+def get_model(model_no):
+	'''
+	Hospital choose to run a particular model
+	'''
+	random_state = 30
+
+	models = get_models()
+
+	if model_no >= 1 and model_no <= 3:
+		return models[model_no]
+	else:
+		#invalid model_no and then in any case the max will be picked in report crunching
+		return models
 
 #Separate train dataset and test dataset
 def generate_train_test_split(transformedDF, test_size):
@@ -58,22 +73,25 @@ def evaluate_model(models, x_train, y_train):
 	kfold = StratifiedKFold(n_splits = 10)
 	
 	result = []
-	for model in models :
+	for mod_num in models.keys():
+		model = models[mod_num]
 		model_cv = cross_val_score(estimator = model[1], X = x_train, y = y_train, scoring = "accuracy", 
 									  cv = kfold, n_jobs=4)
-		result.append(model_cv)
+		result.append([model_cv, mod_num])
 
 	cv_means = []
 	cv_std = []
+	cv_model = []
 	for cv_result in result:
 		#print(cv_result)
-		cv_means.append(cv_result.mean())
-		cv_std.append(cv_result.std())
+		cv_means.append(cv_result[0].mean())
+		cv_std.append(cv_result[0].std())
+		cv_model.append(cv_result[1])
 
 	result_df = pd.DataFrame({
 		"CrossValMeans":cv_means,
 		"CrossValerrors": cv_std,
-		"Models":[m[0] for m in models]
+		"Models":cv_model
 	})
 
 	# Generate chart
@@ -83,32 +101,83 @@ def evaluate_model(models, x_train, y_train):
 	return result_df
 
 
-def fit_and_predict_models(model, x_train, y_train, x_test, y_test):
+def fit_and_predict_model(mod_num, model, x_train, y_train, x_test, y_test):
 	'''
 	Function to train a model, test it and report F1, precision and recall on the test predictions
 	'''
 	model[1].fit(x_train, y_train)
 	y_pred_model = model[1].predict(x_test)
-	print('Model : ' + model[0])
+	#print('Model : ' + model[0])
 	class_report = classification_report(y_test, y_pred_model, output_dict=True)
 	#converting report to dataframe
 	class_report = pd.DataFrame(class_report).T
 	class_report = class_report.set_axis(class_report.columns, axis=1, inplace=False).rename_axis('dimensions',axis=0)
 	class_report.reset_index(inplace=True)
 	#printing and returning report
-	print(class_report)
-	return class_report
+	#print(class_report)
+	clean_class_report_edited = clean_class_report(class_report, len(x_train), len(x_test), mod_num)
+	return clean_class_report_edited
+
+def clean_class_report(class_report, num_x_train, num_x_test, mod_num):
+	'''
+	Just get the classification report dictionary idea is to parse into output the blockchain can store
+	E.g., train_data, test_data, overall_precision, sensitivity, specificity, overall_f1
+	'''
+	class_report_edited = {'model number': mod_num,
+							'train size': num_x_train, 
+							'test size': num_x_test,
+							'test precision': round(class_report[class_report['dimensions'] == 'weighted avg']['precision'].item(), 2),
+							'test f1': round(class_report[class_report['dimensions'] == 'weighted avg']['f1-score'].item(), 2),
+							'test sensitivity (recall - yes diabetes)': round(class_report[class_report['dimensions'] == '1.0']['recall'].item(), 2),
+							'test speficity (recall - no diabetes)': round(class_report[class_report['dimensions'] == '0.0']['recall'].item(), 2)
+							}
+
+	return class_report_edited
+
+
+def get_model_output(models_output, pick_max, pick_other_node_model):
+	'''
+	
+	models_output contains the name of the model not the number 
+	'''
+
+	if pick_other_node_model >= 1 and pick_other_node_model <= 3:
+		return models_output[pick_other_node_model]
+	else:
+		#pick the max model 
+		max_f1 = 0
+		max_model = 0
+
+		for model in models_output.keys():
+			class_report_model = models_output[model]
+
+			if class_report_model['test f1'] > max_f1:
+				max_model = model
+
+		if max_model != 0:
+			return models_output[max_model]
+		else:
+			return models_output[1]
+	
+
 
 if __name__ == '__main__':
-	pima_diabetes = pd.read_csv('../data/hospital1.csv', index_col=0)
+	pima_diabetes = pd.read_csv('../data/hospital2.csv', index_col=0)
 	transformedDF = transform_data(pima_diabetes)
 
 	models = get_models()
 
 	x_train, x_test, y_train, y_test = generate_train_test_split(transformedDF, 0.30)
 	evaluate_model(models, x_train, y_train)
+	model_output = {}
 
-	for model in models:
-		fit_and_predict_models(model, x_train, y_train, x_test, y_test)
+	for mod_num in models.keys():
+		model = models[mod_num]
+
+		mod_classification_report = fit_and_predict_model(mod_num, model, x_train, y_train, x_test, y_test)
+		model_output[mod_num] = mod_classification_report
+
+	print(model_output)
+	print(get_model_output(model_output, True, 0))
 
 
